7cd6998481b52526217259d733b9e39b64c2cc4d
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-17 08:47:50 -0300

    Resolving conflicts

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ import userRoutes from './routes/users.js';
 const app = express();
 
 dotenv.config();
-app.use(express.json({ limit: "30mb", extended: true}));
-app.use(express.urlencoded({ limit: "30mb", extended: true}));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 app.use('/posts', postRoutes);
@@ -23,8 +23,8 @@ app.get('/', (req, res) => {
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+    .catch((error) => console.error(`DB Error: ${error.message}`));
 
 mongoose.set('useFindAndModify', false);
